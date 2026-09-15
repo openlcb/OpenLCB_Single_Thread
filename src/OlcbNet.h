f627432815cc8a7d799226f8b01db4e364d536ab
David Harris <drdpharris@gmail.com>
2026-09-14 19:19:20 -0700

    Make OlcbNet methods pure virtual

--- src/OlcbNet.h
+++ src/OlcbNet.h
@@ -15,11 +15,11 @@
 
 class OlcbNet {
   public:
-    virtual void init();                    // initialization
-    virtual uint8_t avail();                // read rxbuffer available
-    virtual uint8_t read();                 // read a buffer
-    virtual uint8_t txReady();              // write txbuffer available
-    virtual uint8_t write(long timeout);    // write, 0= immediately or fail; 0< if timeout occurs fail
+    virtual void init() = 0;                    // initialization
+    virtual uint8_t avail() = 0;                // read rxbuffer available
+    virtual uint8_t read() = 0;                 // read a buffer
+    virtual uint8_t txReady() = 0;              // write txbuffer available
+    virtual uint8_t write(long timeout) = 0;    // write, 0= immediately or fail = 0; 0< if timeout occurs fail
     virtual uint8_t write() {               // write(0), ie write immediately
         return write((long) 0);
     };
